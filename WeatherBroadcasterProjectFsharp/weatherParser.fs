6c8dfa4e098d75c1629ac4f4324f0a7551acb7b1
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-04-01 17:05:41 +0300

    Update weatherParser.fs

--- WeatherBroadcasterProjectFsharp/weatherParser.fs
+++ WeatherBroadcasterProjectFsharp/weatherParser.fs
@@ -3,7 +3,7 @@ open FSharp.Data
 module weatherParser =
 
 type Json = JsonProvider<"sample.json">
-let API_KEY = "b1b15e88fa797225412429c1c50c122a1" // PUT YOUR API KEY HERE
+let API_KEY = "YourAPIKey" // PUT YOUR API KEY HERE
 let get_info (city: string) = Http.RequestString("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY + "&units=metric" )
 let Json_Info info  = Json.Parse(info.ToString())
 
